2da7df4ab1d7011bbdc96df5fbc237338604c100
unknown <pedroescapalete@gmail.com>
2025-04-11 15:58:57 -0300

    while

--- while/ex08.js
+++ while/ex08.js
@@ -1,6 +1,6 @@
 let numero = 1;
 
-while(numero <= 99){
+while(numero <= 4){
     numero++
     let resultado = numero * numero * numero
     console.log(`O numero ${numero}³, ao cubo é ${resultado}`)
